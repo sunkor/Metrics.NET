3a519df60b7d9064fc5c00d14881e3ff5600e466
Igal Tabachnik <hmemcpy@gmail.com>
2016-04-07 16:11:15 +0300

    Adding a nuspec for Metrics.NLog package

--- Publishing/push-nuget.bat
+++ Publishing/push-nuget.bat
@@ -1,3 +1,4 @@
 ..\.nuget\NuGet.exe push Metrics.NET.0.2.16.nupkg
 ..\.nuget\NuGet.exe push Nancy.Metrics.0.2.16.nupkg
 ..\.nuget\NuGet.exe push Owin.Metrics.0.1.19-alpha.nupkg
+..\.nuget\NuGet.exe push Metrics.NLog.0.2.16.nupkg
