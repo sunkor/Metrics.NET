40effe4ec9f3cd4ffc67fd9f1e7aacd2dae69584
Amir Sasson <Amir.Sasson@888holdings.com>
2016-04-17 16:40:58 +0300

    Supporting Rolling Index for Elastic Reporter

--- Src/Metrics/ElasticSearch/ElasticSearchConfigExtensions.cs
+++ Src/Metrics/ElasticSearch/ElasticSearchConfigExtensions.cs
@@ -13,5 +13,14 @@ namespace Metrics
             return reports.WithReport(new ElasticSearchReport(bulkUri, index, nodeInfoUri), interval);
         }
 
+        public static MetricsReports WithElasticSearch(this MetricsReports reports, ElasticReportsConfig reportConfig, TimeSpan interval)
+        {
+            var uri = new Uri(string.Format(@"http://{0}:{1}/_bulk", reportConfig.Host, reportConfig.Port));
+            var nodeInfoUri = new Uri(string.Format(@"http://{0}:{1}", reportConfig.Host, reportConfig.Port));
+
+            //return reports.WithReport(new ElasticSearchReport(bulkUri, index, nodeInfoUri), interval);
+            return reports.WithReport(new ElasticSearchReport(uri, reportConfig.Index, nodeInfoUri, reportConfig.RollingIndexType), interval);
+        }
+
     }
 }
