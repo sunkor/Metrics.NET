632bf856c8cfdfb19ff9ccec5dc0dda2581c8dc7
Amir Sasson <Amir.Sasson@888holdings.com>
2016-05-10 08:21:20 +0300

    Refactor Health reporting on elastic

--- Src/Metrics/ElasticSearch/ElasticReportsConfig.cs
+++ Src/Metrics/ElasticSearch/ElasticReportsConfig.cs
@@ -1,9 +1,24 @@
-﻿namespace Metrics.ElasticSearch
+﻿using System;
+using System.Reflection;
+
+namespace Metrics.ElasticSearch
 {
+    /// <summary>
+    /// Manipulates the Elastic to be broken into partitions for maintenance purposes
+    /// </summary>
     public enum RollingIndexType
     {
+        /// <summary>
+        /// the Elastic Index would be the same as provided to Config
+        /// </summary>
         None,
+        /// <summary>
+        /// the Elastic Index would be the INDEX-yyyy-MM-dd, use this to be able to delete Index by day for maintenance purposes
+        /// </summary>
         Daily,
+        /// <summary>
+        /// the Elastic Index would be the INDEX-yyyy-MM, use this to be able to delete Index by month for maintenance purposes
+        /// </summary>
         Monthly
     }
 
@@ -14,5 +29,37 @@
         public string Index { get; set; }
 
         public RollingIndexType RollingIndexType { get; set; }
+        /// <summary>
+        /// optional, provide this interface to be reflected on the health checks document reports
+        /// </summary>
+        public IApplicationInfo ReportingApplication { get; set; }
     }
+
+    public interface IApplicationInfo
+    {
+        string Name { get; }
+        string Version { get; }
+        DateTime StartTime { get; }
+        TimeSpan UpTime { get; }
+    }
+
+    public class ApplicationInfo : IApplicationInfo
+    {
+        public ApplicationInfo(string name, string version)
+        {
+            StartTime = DateTime.UtcNow;
+            Name = name;
+            Version = version;
+        }
+        public ApplicationInfo() : this(Assembly.GetCallingAssembly().GetName().Name, Assembly.GetCallingAssembly().GetName().Version.ToString())
+        { }
+
+        public string Name { get; private set; }
+        public DateTime StartTime { get; private set; }
+        public TimeSpan UpTime { get { return DateTime.UtcNow - StartTime; } }
+        public string Version { get; private set; }
+
+
+    }
+
 }
--- Src/Metrics/ElasticSearch/ElasticSearchConfigExtensions.cs
+++ Src/Metrics/ElasticSearch/ElasticSearchConfigExtensions.cs
@@ -6,19 +6,15 @@ namespace Metrics
 {
     public static class ElasticSearchConfigExtensions
     {
+        [Obsolete("Use WithElasticSearch(ElasticReportsConfig) instead")]
         public static MetricsReports WithElasticSearch(this MetricsReports reports, string host, int port, string index, TimeSpan interval)
         {
-            var bulkUri = new Uri($@"http://{host}:{port}/_bulk");
-            var nodeInfoUri = new Uri($@"http://{host}:{port}");
-            return reports.WithReport(new ElasticSearchReport(bulkUri, index, nodeInfoUri), interval);
+            return reports.WithElasticSearch(new ElasticReportsConfig() { Index = index, Host = host, RollingIndexType = ElasticReportsConfig.RollingIndex.None, Port = port }, interval);
         }
 
         public static MetricsReports WithElasticSearch(this MetricsReports reports, ElasticReportsConfig reportConfig, TimeSpan interval)
         {
-            var bulkUri = new Uri($@"http://{reportConfig.Host}:{reportConfig.Port}/_bulk");
-            var nodeInfoUri = new Uri($@"http://{reportConfig.Host}:{reportConfig.Port}");
-
-            return reports.WithReport(new ElasticSearchReport(bulkUri, reportConfig.Index, nodeInfoUri, reportConfig.RollingIndexType), interval);
+            return reports.WithReport(new ElasticSearchReport(reportConfig), interval);
         }
 
     }
