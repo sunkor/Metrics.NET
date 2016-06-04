0e2082e653638b9fb39790c0efac0918ed19d556
Paul Parau <paul.parau@recognos.ro>
2016-06-04 16:21:58 +0300

    Add MetricsEndpointReport configuration class

---
+++ Src/Metrics/Reporters/MetricsEndpointReports.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using Metrics.Visualization;
+
+namespace Metrics.Reports
+{
+    public sealed class MetricsEndpointReports : Utils.IHideObjectMembers
+    {
+        private readonly List<MetricsEndpoint> endpoints = new List<MetricsEndpoint>();
+
+        internal IReadOnlyList<MetricsEndpoint> Endpoints => this.endpoints;
+
+        public MetricsEndpointReports()
+        {
+        }
+
+        public MetricsEndpointReports WithEndpointReport(string endpoint, Func<string> contentFactory)
+        {
+            var metricsEndpoint = new MetricsEndpoint(endpoint, contentFactory, "text/plain");
+            this.endpoints.Add(metricsEndpoint);
+            return this;
+        }
+    }
+}
---
+++ Src/Metrics/Visualization/MetricsEndpoint.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Metrics.Visualization
+{
+    public sealed class MetricsEndpoint
+    {
+        private readonly Func<string> contentFactory;
+
+        public readonly string Endpoint;
+        public readonly string ContentType;
+        public string ProduceContent() => this.contentFactory();
+
+        public MetricsEndpoint(string endpoint, Func<string> contentFactory, string contentType)
+        {
+            this.Endpoint = endpoint;
+            this.ContentType = contentType;
+
+            this.contentFactory = contentFactory;
+        }
+    }
+}
