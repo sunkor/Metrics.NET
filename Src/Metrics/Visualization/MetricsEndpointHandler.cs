afbf57138fbcb687ff4887c8a8ff63176809e9da
Paul Parau <paul.parau@recognos.ro>
2016-09-17 14:43:34 +0300

    Added AbstractMetricsEndpointHandler

--- Src/Metrics/Visualization/MetricsEndpointHandler.cs
+++ Src/Metrics/Visualization/MetricsEndpointHandler.cs
@@ -4,33 +4,14 @@ using System.Net;
 
 namespace Metrics.Visualization
 {
-    public sealed class MetricsEndpointHandler
+    public sealed class MetricsEndpointHandler : AbstractMetricsEndpointHandler<HttpListenerContext>
     {
-        private readonly MetricsEndpoint[] endpoints;
+        public MetricsEndpointHandler(IEnumerable<MetricsEndpoint> endpoints) : base(endpoints) { }
 
-        public MetricsEndpointHandler(IEnumerable<MetricsEndpoint> endpoints)
+        protected override MetricsEndpointRequest CreateRequest(HttpListenerContext requestInfo)
         {
-            this.endpoints = endpoints.ToArray();
-        }
-
-        public MetricsEndpointResponse Process(string urlPath, HttpListenerContext context)
-        {
-            foreach (var endpoint in this.endpoints)
-            {
-                if (endpoint.IsMatch(urlPath))
-                {
-                    var request = CreateRequest(context);
-                    return endpoint.ProduceResponse(request);
-                }
-            }
-
-            return null;
-        }
-
-        private static MetricsEndpointRequest CreateRequest(HttpListenerContext context)
-        {
-            var headers = context.Request.Headers
-                .AllKeys.ToDictionary(key => key, key => context.Request.Headers[key]);
+            var headers = requestInfo.Request.Headers
+                .AllKeys.ToDictionary(key => key, key => requestInfo.Request.Headers[key]);
 
             return new MetricsEndpointRequest(headers);
         }
