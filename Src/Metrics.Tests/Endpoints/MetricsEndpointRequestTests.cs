843e1cbc8f396c18c56988a2d12681d584561f5b
Paul Parau <paul.parau@recognos.ro>
2016-12-02 21:00:50 +0200

    Added some more unit tests

---
+++ Src/Metrics.Tests/Endpoints/MetricsEndpointRequestTests.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using FluentAssertions;
+using Metrics.Endpoints;
+using Xunit;
+
+namespace Metrics.Tests.Endpoints
+{
+    public class MetricsEndpointRequestTests
+    {
+        [Fact]
+        public void MetricsEndpointRequest_CannotCreateWithNullHeaders()
+        {
+            var action = new Action(() => new MetricsEndpointRequest(null));
+            action.ShouldThrow<Exception>();
+        }
+    }
+}
---
+++ Src/Metrics.Tests/Endpoints/MetricsEndpointResponseTests.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Text;
+using FluentAssertions;
+using Metrics.Endpoints;
+using Xunit;
+
+namespace Metrics.Tests.Endpoints
+{
+    public class MetricsEndpointResponseTests
+    {
+        [Fact]
+        public void MetricsEndpointResponse_HasSaneDefaults()
+        {
+            var response = new MetricsEndpointResponse("content", "content-type");
+            response.Content.Should().Be("content");
+            response.ContentType.Should().Be("content-type");
+            response.Encoding.Should().Be(Encoding.UTF8);
+            response.StatusCode.Should().Be(200);
+            response.StatusCodeDescription.Should().Be("OK");
+
+            response = new MetricsEndpointResponse("content", "content-type", Encoding.ASCII);
+            response.Content.Should().Be("content");
+            response.ContentType.Should().Be("content-type");
+            response.Encoding.Should().Be(Encoding.ASCII);
+            response.StatusCode.Should().Be(200);
+            response.StatusCodeDescription.Should().Be("OK");
+        }
+
+        [Fact]
+        public void MetricsEndpointResponse_CannotCreateWithoutContent()
+        {
+            var action = new Action(() => new MetricsEndpointResponse(null, "content-type", Encoding.ASCII));
+            action.ShouldThrow<Exception>();
+        }
+
+        [Fact]
+        public void MetricsEndpointResponse_CannotCreateWithInvalidContentType()
+        {
+            var action1 = new Action(() => new MetricsEndpointResponse("content", null, Encoding.ASCII));
+            var action2 = new Action(() => new MetricsEndpointResponse("content", string.Empty, Encoding.ASCII));
+            var action3 = new Action(() => new MetricsEndpointResponse("content", " ", Encoding.ASCII));
+            action1.ShouldThrow<Exception>();
+            action2.ShouldThrow<Exception>();
+            action3.ShouldThrow<Exception>();
+        }
+
+        [Fact]
+        public void MetricsEndpointResponse_CannotCreateWithoutEncoding()
+        {
+            var action = new Action(() => new MetricsEndpointResponse("content", "content-type", null));
+            action.ShouldThrow<Exception>();
+        }
+
+        [Fact]
+        public void MetricsEndpointResponse_CannotCreateWithoutStatusCodeDescription()
+        {
+            var action = new Action(() => new MetricsEndpointResponse("content", "content-type", Encoding.ASCII, 200, null));
+            action.ShouldThrow<Exception>();
+        }
+    }
+}
